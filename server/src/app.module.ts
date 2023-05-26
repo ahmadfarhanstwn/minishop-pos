b5eac562ee93b4ff7f424c9220d6c2c6dcfb1b13
Ahmad Farhan <ahmadfarhanstwn@gmail.com>
2023-05-27 01:51:23 +0700

    fix docker compose and generate auth module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -21,7 +22,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         entities: [],
         synchronize: false,
       })
-    })
+    }),
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
